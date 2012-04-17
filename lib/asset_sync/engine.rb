d1c6f03b2efe8dcec85f0de49c05490d82d4947c
Mac book Air <jchappypig@hotmail.com>
2012-04-17 11:33:38 +1000

    added log to printout the asset sync config.

--- lib/asset_sync/engine.rb
+++ lib/asset_sync/engine.rb
@@ -15,15 +15,25 @@ class Engine < Rails::Engine
         config.fog_provider = ENV['FOG_PROVIDER']
         config.fog_directory = ENV['FOG_DIRECTORY']
         config.fog_region = ENV['FOG_REGION']
+        AssetSync.log "AssetSync: config.fog_provider #{config.fog_provider}"
+        AssetSync.log "AssetSync: config.fog_directory #{config.fog_directory}"
+        AssetSync.log "AssetSync: config.fog_region #{config.fog_region}"
 
         config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
         config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+        AssetSync.log "AssetSync: config.aws_access_key_id #{config.aws_access_key_id}"
+        AssetSync.log "AssetSync: config.aws_secret_access_key #{config.aws_secret_access_key}"
 
         config.rackspace_username = ENV['RACKSPACE_USERNAME']
         config.rackspace_api_key = ENV['RACKSPACE_API_KEY']
         config.existing_remote_files = ENV['ASSET_SYNC_EXISTING_REMOTE_FILES'] || "keep"
         config.gzip_compression = ENV['ASSET_SYNC_GZIP_COMPRESSION'] == 'true'
         config.manifest = ENV['ASSET_SYNC_MANIFEST'] == 'true'
+        AssetSync.log "AssetSync: config.rackspace_username #{config.rackspace_username}"
+        AssetSync.log "AssetSync: config.rackspace_api_key #{config.rackspace_api_key}"
+        AssetSync.log "AssetSync: config.existing_remote_files #{config.existing_remote_files}"
+        AssetSync.log "AssetSync: config.gzip_compression #{config.gzip_compression}"
+        AssetSync.log "AssetSync: config.manifest #{config.manifest}"
       end
     end
 
